d1763fa2565664f7abca502b88fee2fc0d687ee4
Sap3X <Sapex1347@gmail.com>
2024-09-21 23:11:54 +0530

    changes

--- Admin/php/login.php
+++ Admin/php/login.php
@@ -7,18 +7,18 @@
         {
 			if($password=="admin")
             {
-				echo "<script>window.location='../html/Adminoption.html'</script>";
+				echo "<script>window.location='../html/Option.html'</script>";
 			}
 			else
             {
 				echo"<script>alert('Invalid Password')</script>";
-				echo "<script>window.location='../Adminlogin.html'</script>";
+				echo "<script>window.location='../Login.html'</script>";
 			}
 		}
 		else
         {
 			echo "<script>alert('Invalid Username')</script>";
-			echo "<script>window.location='../Adminlogin.html'</script>";
+			echo "<script>window.location='../Login.html'</script>";
 		}
 	}
 ?>
